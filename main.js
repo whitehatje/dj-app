ff1954ff33c29f14451c443af3f2ae5d849f4b0c
whitehatje <108880609+whitehatje@users.noreply.github.com>
2022-07-28 17:47:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
 
 function setup()
 {
@@ -11,7 +12,7 @@ canvas.center();
 video = createCapture(VIDEO);
 video.hide();
 
-posenet = ml5.posenet(video , modelLoaded);
+posenet = ml5.poseNet(video , modelLoaded);
 posenet.on('pose' , gotPoses);
 }
 
@@ -20,6 +21,8 @@ function gotPoses(results)
 if (results.length > 0 )
 {
 console.log(results);
+scoreLeftWrist = results[0].pose.keypoint[9].score;
+console.log("Score Left Wrist = " + scoreLeftWrist);
 leftWristX = results[0].pose.leftWrist.x;
 leftWristY = results[0].pose.leftWrist.y; 
 console.log("Left Wrist x =" + leftWristX + "Left Wrist y =" + leftWristY);
@@ -39,6 +42,19 @@ console.log("posenet is initialized")
 function draw()
 {
     image(video , 0 , 0 , 600 , 500);
+
+    fill("#FF0000");
+    stroke("#FF0000");
+    if(scoreLeftWrist > 0.2)
+    {
+    circle(leftWristX , leftWristY , 20);
+
+    InNumberLeftWristY = Number(leftWristY);
+    Remove_decimals = floor(InNumberLeftWristY);
+    volume = Remove_decimals/500;
+    document.getElementById("volume").innerHTML = "volume = " + volume;
+    song.setVolume(volume);
+    }
 }
 
 function preload()
